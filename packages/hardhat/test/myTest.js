142052fdb58dac3d15ede53cd8325bfbbbbcf0b3
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2022-10-27 00:00:04 +0200

    doing stuff with bytes+

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -2,25 +2,39 @@ const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 
+const BN = require('bn.js');
+
+// Enable and inject BN dependency
+use(require('chai-bn')(BN));
+
 use(solidity);
 
 describe("My Dapp", function () {
   let myContract;
 
+  async function deployContract(){
+    const YourContract = await ethers.getContractFactory("YourCollectible");
+
+    myContract = await YourContract.deploy();
+
+  }
+
   describe("YourContract", function () {
     it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
-
-      myContract = await YourContract.deploy();
+      await deployContract()
     });
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+    it("Should initialize a mock state", async function () {
+      // const newPurpose = "Test Purpose";
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
+      // await myContract.setPurpose(newPurpose);
+      // expect(await myContract.purpose()).to.equal(newPurpose);
+      const mockState = await myContract.mockState()
+
+      expect(mockState).to.be.a.bignumber
+
     });
+
   });
+
 });
