aa6a48132119a76f4ddb0ed63a7ae738918e73c3
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2022-12-01 16:13:01 +0100

    baby steps

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -126,42 +126,44 @@ function ItemCard(props) {
   };
 
   return (
-    <Card
-      style={{ margin: "auto", borderRadius: "0", border: "2px solid #c3c3c3", overflow: "hidden" }}
-      onClick={flipCard}
-    >
-      {isFront ? (
-        <img src={item.image} alt="g0l" />
-      ) : (
-        <div style={{ width: "320px", height: "320px", margin: "auto" }}>
-          <div>{item.description}</div>
-          <div>owned by: {item.owner}</div>
-          <div>traits: placeholder</div>
-          <AddressInput
-            ensProvider={ensProvider}
-            placeholder="transfer to address"
-            value={transferToAddresses[item.id]}
-            onChange={newValue => {
-              // e.stopPropagation();
-              const update = {};
-              update[item.id] = newValue;
-              setTransferToAddresses({ ...transferToAddresses, ...update });
-            }}
-            onClick={e => {
-              e.stopPropagation();
-            }}
-          />
-          <Button
-            onClick={e => {
-              e.stopPropagation();
-              tx(writeContracts.YourCollectible.transferFrom(address, transferToAddresses[item.id], item.id));
-            }}
-          >
-            Transfer
-          </Button>
-        </div>
-      )}
-    </Card>
+    <div>
+      <Card
+        style={{ margin: "auto", borderRadius: "0", border: "2px solid #c3c3c3", overflow: "hidden" }}
+        onClick={flipCard}
+      >
+        {isFront ? (
+          <img src={item.image} alt="g0l" />
+        ) : (
+          <div style={{ width: "320px", height: "320px", margin: "auto" }}>
+            <div>{item.description}</div>
+            <div>owned by: {item.owner}</div>
+            <div>traits: placeholder</div>
+            <AddressInput
+              ensProvider={ensProvider}
+              placeholder="transfer to address"
+              value={transferToAddresses[item.id]}
+              onChange={newValue => {
+                // e.stopPropagation();
+                const update = {};
+                update[item.id] = newValue;
+                setTransferToAddresses({ ...transferToAddresses, ...update });
+              }}
+              onClick={e => {
+                e.stopPropagation();
+              }}
+            />
+            <Button
+              onClick={e => {
+                e.stopPropagation();
+                tx(writeContracts.YourCollectible.transferFrom(address, transferToAddresses[item.id], item.id));
+              }}
+            >
+              Transfer
+            </Button>
+          </div>
+        )}
+      </Card>
+    </div>
   );
 }
 
