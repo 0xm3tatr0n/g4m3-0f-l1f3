cb5f5e6886a901302146652c2c442d24f76b6260
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2022-12-21 14:39:17 +0100

    mint many@deploy time working now

--- packages/hardhat/contracts/Libraries.sol
+++ packages/hardhat/contracts/Libraries.sol
@@ -58,9 +58,7 @@ library G0l {
             // not in use yet:
             // monochrome
             ["#061A40", "#003559", "#B9D6F2", "#006DAA", "#B9D6F2", "#003559", "#BFD7EA"],
-            ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff"],
-
-
+            ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff"]
         ];
         
 
