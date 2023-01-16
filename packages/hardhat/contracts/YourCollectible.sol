e97a23a17c9ffc04e5a54464f350de4e2ce93caa
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2023-01-16 14:13:24 +0100

    prettier working

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -2,11 +2,11 @@ pragma solidity >=0.7.0 <0.8.0;
 //SPDX-License-Identifier: MIT
 
 import 'hardhat/console.sol';
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Pausable.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 import 'base64-sol/base64.sol';
 
 import './HexStrings.sol';
@@ -20,65 +20,55 @@ import {G0l, BitOps} from './Libraries.sol';
 import {Structs} from './StructsLibrary.sol';
 
 contract YourCollectible is ERC721, Pausable, Ownable, G4m3 {
-
   using Strings for uint256;
   using HexStrings for uint160;
+
   // using ToColor for bytes3;
 
-  constructor() ERC721("gam3 0f l1f3", "g0l") {
+  constructor() ERC721('gam3 0f l1f3', 'g0l') {
     createTime = block.timestamp;
     _initState();
   }
 
   function pause() public onlyOwner {
-      _pause();
+    _pause();
   }
 
   function unpause() public onlyOwner {
-      _unpause();
+    _unpause();
   }
 
   // events
   event Withdrawal(address to, uint256 amount);
 
   // constants
-  uint256 constant public maxItems = 10;
-  uint256 constant public mintPrice = 0.01 ether;
-
+  uint256 public constant maxItems = 10;
+  uint256 public constant mintPrice = 0.01 ether;
 
   // variables
   uint256 private minted4free = 0;
   uint256 private createTime;
 
-  function mintItem(address mintTo)
-      public
-      payable
-      whenNotPaused
-      returns (uint256)
-  {
-      require( msg.value >= mintPrice, "No such thing as a free mint!");
-      tokenIdsIncrement();
+  function mintItem(address mintTo) public payable whenNotPaused returns (uint256) {
+    require(msg.value >= mintPrice, 'No such thing as a free mint!');
+    tokenIdsIncrement();
 
-      uint256 id = tokenIdsCurrent();
-      _mint(mintTo, id);
-      _iterateState();
+    uint256 id = tokenIdsCurrent();
+    _mint(mintTo, id);
+    _iterateState();
 
-      // store token states
-      tokenGridStatesInt[id] = gameStateInt;
-      tokenGeneration[id] = generationCurrent();
+    // store token states
+    tokenGridStatesInt[id] = gameStateInt;
+    tokenGeneration[id] = generationCurrent();
 
-      return id;
+    return id;
   }
 
-  function mintMany(address mintTo, uint256 noItems) 
-    public 
-    payable 
-    whenNotPaused 
-  {
-    require(noItems <= maxItems, "too many mints requested");
-    require(msg.value >= mintPrice * noItems, "not enough funds sent");
+  function mintMany(address mintTo, uint256 noItems) public payable whenNotPaused {
+    require(noItems <= maxItems, 'too many mints requested');
+    require(msg.value >= mintPrice * noItems, 'not enough funds sent');
 
-    for (uint i = 0; i < noItems; i++){
+    for (uint256 i = 0; i < noItems; i++) {
       tokenIdsIncrement();
 
       uint256 id = tokenIdsCurrent();
@@ -90,62 +80,62 @@ contract YourCollectible is ERC721, Pausable, Ownable, G4m3 {
       tokenGeneration[id] = generationCurrent();
     }
   }
-  
-  function tokenURI(uint256 id) public view override returns (string memory) {
-      require(_exists(id), "token does not exist");
-      string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
-      Structs.MetaData memory metadata = generateMetadata(id);
 
-      return
-          string(
+  function tokenURI(uint256 id) public view override returns (string memory) {
+    require(_exists(id), 'token does not exist');
+    string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
+    Structs.MetaData memory metadata = generateMetadata(id);
+
+    return
+      string(
+        abi.encodePacked(
+          'data:application/json;base64,',
+          Base64.encode(
+            bytes(
               abi.encodePacked(
-                'data:application/json;base64,',
-                Base64.encode(
-                    bytes(
-                          abi.encodePacked(
-                              '{"name":"',
-                              metadata.name,
-                              '", "description":"',
-                              metadata.description,
-                              '", "external_url":"https://burnyboys.com/token/',
-                              id.toString(),
-                              generateAttributeString(metadata),
-                              '"owner":"',
-                              (uint160(ownerOf(id))).toHexString(20),
-                              '", "image": "',
-                              'data:image/svg+xml;base64,',
-                              image,
-                              '"}'
-                          )
-                        )
-                    )
+                '{"name":"',
+                metadata.name,
+                '", "description":"',
+                metadata.description,
+                '", "external_url":"https://burnyboys.com/token/',
+                id.toString(),
+                generateAttributeString(metadata),
+                '"owner":"',
+                (uint160(ownerOf(id))).toHexString(20),
+                '", "image": "',
+                'data:image/svg+xml;base64,',
+                image,
+                '"}'
               )
-          );
+            )
+          )
+        )
+      );
   }
 
   // Visibility is `public` to enable it being called by other contracts for composition.
-function withdrawAmount(uint256 amount) public onlyOwner {
-  require(amount <= address(this).balance, "withdraw amnt too high");
-  msg.sender.transfer(amount);
-  emit Withdrawal(msg.sender, amount);
-}
+  function withdrawAmount(uint256 amount) public onlyOwner {
+    require(amount <= address(this).balance, 'withdraw amnt too high');
+    msg.sender.transfer(amount);
+    emit Withdrawal(msg.sender, amount);
+  }
 
-function mintForFree(address mintTo, uint256 noItems) public onlyOwner {
-  // owner mint allocation of 1 item / day
-  uint256 currentAlloc = (block.timestamp - createTime) / (1 days);
-  require((minted4free + noItems) <= currentAlloc, "not enough free mints");
+  function mintForFree(address mintTo, uint256 noItems) public onlyOwner {
+    // owner mint allocation of 1 item / day
+    uint256 currentAlloc = (block.timestamp - createTime) / (1 days);
+    require((minted4free + noItems) <= currentAlloc, 'not enough free mints');
 
-  for (uint i = 0; i < noItems; i++){
-    tokenIdsIncrement();
+    for (uint256 i = 0; i < noItems; i++) {
+      tokenIdsIncrement();
 
-    uint256 id = tokenIdsCurrent();
-    _mint(mintTo, id);
-    minted4free += 1;
-    _iterateState();
+      uint256 id = tokenIdsCurrent();
+      _mint(mintTo, id);
+      minted4free += 1;
+      _iterateState();
 
-    // store token states
-    tokenGridStatesInt[id] = gameStateInt;
-    tokenGeneration[id] = generationCurrent();
-  } 
-}
+      // store token states
+      tokenGridStatesInt[id] = gameStateInt;
+      tokenGeneration[id] = generationCurrent();
+    }
+  }
 }
