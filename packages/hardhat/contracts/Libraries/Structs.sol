15342398e47f79156bf1b3b5f38379b9d447df34
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2023-02-28 18:42:49 +0100

    maybe much better

--- packages/hardhat/contracts/Libraries/Structs.sol
+++ packages/hardhat/contracts/Libraries/Structs.sol
@@ -41,7 +41,7 @@ library Structs {
     uint8 bornCounter;
     uint8 perishedCounter;
     // uint8 representation;
-    uint8 shape; // 0: circles, 1: blocks, 2: blocks/triangles
+    uint8 shape; // 0: circles, 1: blocks, 2: blocks&triangles, 3: blocks & circles
     uint8 speed; // 0: raw, 1: static, 2: slow, 3: medium, 4: fast ,1,2 : lower == slower
     uint8 pattern; // 0: random, 1: matrix, 2: ...
     uint8 i;
