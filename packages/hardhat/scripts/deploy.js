d6f1da7392de967c3310607ae3c72be77f2c96af
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2023-04-03 14:06:07 +0200

    improvements

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -43,7 +43,7 @@ const main = async () => {
     );
 
     // mint a bunch at deploy time to have a collection right away
-    const MINTS_10 = 0; // how many times to mintMany, max 10 per transaction
+    const MINTS_10 = 10; // how many times to mintMany, max 10 per transaction
     for (let i = 0; i < MINTS_10; i++) {
       const minted = await yourCollectible.mintPack('0x9B5d8C94aAc96379e7Bcac0Da7eAA1E8EB504295', {
         value: ethers.utils.parseEther((0.025).toString()),
