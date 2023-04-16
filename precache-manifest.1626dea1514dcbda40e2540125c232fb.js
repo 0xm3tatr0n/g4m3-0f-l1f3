664c9cb2aa0246dd3b536f4e258ee73251774325
0xm3tatr0n <0xm3tatr0n@users.noreply.github.com>
2023-04-16 20:51:12 +0000

    deploy: a48907e2ce1f6c1a423ff124963674ffa196b340

---
+++ precache-manifest.1626dea1514dcbda40e2540125c232fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c2bc3119400d9a27de1be1dc691ac7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7da69345207612886e89",
+    "url": "./static/css/2.d3b8fa64.chunk.css"
+  },
+  {
+    "revision": "4aa03e62176ced1f0dc5",
+    "url": "./static/css/main.6a9a0f53.chunk.css"
+  },
+  {
+    "revision": "7da69345207612886e89",
+    "url": "./static/js/2.3ec3977f.chunk.js"
+  },
+  {
+    "revision": "8e07c77cbd477d030c434a99472428be",
+    "url": "./static/js/2.3ec3977f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa03e62176ced1f0dc5",
+    "url": "./static/js/main.c491b634.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.c491b634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c576f31f83f6843e5e",
+    "url": "./static/js/runtime-main.2c954595.js"
+  }
+]);
