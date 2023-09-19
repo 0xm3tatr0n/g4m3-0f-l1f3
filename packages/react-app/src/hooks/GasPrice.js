5b02c909bedf6f686a9b8f44d978298648416687
m3tatr0n <95829052+0xm3tatr0n@users.noreply.github.com>
2023-09-19 16:19:03 +0200

    replaced ethgasstation with etherscan, no API keys used

--- packages/react-app/src/hooks/GasPrice.js
+++ packages/react-app/src/hooks/GasPrice.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { usePoller } from "eth-hooks";
 import { useState } from "react";
+import { ETHERSCAN_KEY } from "../constants";
 
-export default function useGasPrice(targetNetwork, speed) {
+export default function useGasPrice(targetNetwork, speed, pollTime = 39999) {
   const [gasPrice, setGasPrice] = useState();
   const loadGasPrice = async () => {
-    if (targetNetwork.gasPrice) {
+    if (targetNetwork.hasOwnProperty("gasPrice")) {
       setGasPrice(targetNetwork.gasPrice);
     } else {
       axios
-        .get("https://ethgasstation.info/json/ethgasAPI.json")
+        .get("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + ETHERSCAN_KEY)
         .then(response => {
-          const newGasPrice = response.data[speed || "fast"] * 100000000;
+          console.log("response gas: ", response);
+          const newGasPrice = response.data.result[speed || "FastGasPrice"] * 1000000000;
           if (newGasPrice !== gasPrice) {
             setGasPrice(newGasPrice);
           }
@@ -23,3 +25,30 @@ export default function useGasPrice(targetNetwork, speed) {
   usePoller(loadGasPrice, 39999);
   return gasPrice;
 }
+
+// old, failing version:
+// import axios from "axios";
+// import { usePoller } from "eth-hooks";
+// import { useState } from "react";
+
+// export default function useGasPrice(targetNetwork, speed) {
+//   const [gasPrice, setGasPrice] = useState();
+//   const loadGasPrice = async () => {
+//     if (targetNetwork.gasPrice) {
+//       setGasPrice(targetNetwork.gasPrice);
+//     } else {
+//       axios
+//         .get("https://ethgasstation.info/json/ethgasAPI.json")
+//         .then(response => {
+//           const newGasPrice = response.data[speed || "fast"] * 100000000;
+//           if (newGasPrice !== gasPrice) {
+//             setGasPrice(newGasPrice);
+//           }
+//         })
+//         .catch(error => console.log(error));
+//     }
+//   };
+
+//   usePoller(loadGasPrice, 39999);
+//   return gasPrice;
+// }
