f7b54e9df0640f114f55b518ecb4aa3c49de2fb1
0xm3tatr0n <0xm3tatr0n@users.noreply.github.com>
2023-10-02 13:48:14 +0000

    deploy: cf549dd636d24c8f7ae11d1bd73ddf22dc03d67a

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/g4m3-0f-l1f3/precache-manifest.d849f7c958310f3e4ba1d2a58922296c.js"
+  "/g4m3-0f-l1f3/precache-manifest.a93e5ccd3afbf2dee2c5b90861ffb61d.js"
 );
 
 self.addEventListener('message', (event) => {
