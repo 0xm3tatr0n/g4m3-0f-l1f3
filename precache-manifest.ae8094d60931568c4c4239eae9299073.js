d0a55235f85cbd8c050f714a46d6eacdcf6c7a8e
0xm3tatr0n <0xm3tatr0n@users.noreply.github.com>
2025-04-09 09:49:53 +0000

    deploy: f70972f6c794826ed2a9e7d157d6287b19f31319

---
+++ precache-manifest.ae8094d60931568c4c4239eae9299073.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dbe7320e7d2ce5c9054f8a24e374e4",
+    "url": "/g4m3-0f-l1f3/index.html"
+  },
+  {
+    "revision": "916c56912abb0ee6b1c1",
+    "url": "/g4m3-0f-l1f3/static/css/2.324ccd6c.chunk.css"
+  },
+  {
+    "revision": "6404d0e436003ad630b6",
+    "url": "/g4m3-0f-l1f3/static/css/main.a027c21b.chunk.css"
+  },
+  {
+    "revision": "916c56912abb0ee6b1c1",
+    "url": "/g4m3-0f-l1f3/static/js/2.8e7ff522.chunk.js"
+  },
+  {
+    "revision": "f3357756594c568e60ef683f0249dbfc",
+    "url": "/g4m3-0f-l1f3/static/js/2.8e7ff522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6404d0e436003ad630b6",
+    "url": "/g4m3-0f-l1f3/static/js/main.81a21036.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/g4m3-0f-l1f3/static/js/main.81a21036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c81ffa12001b4754c3",
+    "url": "/g4m3-0f-l1f3/static/js/runtime-main.1872c85c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/g4m3-0f-l1f3/precache-manifest.06081ac5659fcc6431a66b4a6711c602.js"
+  "/g4m3-0f-l1f3/precache-manifest.ae8094d60931568c4c4239eae9299073.js"
 );
 
 self.addEventListener('message', (event) => {
