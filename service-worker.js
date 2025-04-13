92ec4df8df32479327f15a13afda30c2a8e3d54c
0xm3tatr0n <0xm3tatr0n@protonmail.com>
2025-04-13 23:07:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/g4m3-0f-l1f3/precache-manifest.2dd86746eb32190bed2700a7f8b39713.js"
+  "/g4m3-0f-l1f3/precache-manifest.24e17ea0f9a7160b7e0bdca22dd3601f.js"
 );
 
 self.addEventListener('message', (event) => {
